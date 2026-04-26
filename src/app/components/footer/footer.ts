203b11f9bdff655713a7675ca4eb80a181f35521
andregattdev <andregatt.dev@gmail.com>
2026-04-26 12:12:27 -0300

    correções, adiçoes e atualizações

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,14 +1,20 @@
-
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+// ADICIONE ESTA IMPORTAÇÃO:
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatButtonModule, MatIconModule],
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule, FontAwesomeModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {
   readonly year = new Date().getFullYear();
+  
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
 }
